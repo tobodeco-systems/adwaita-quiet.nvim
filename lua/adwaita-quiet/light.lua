748a1ea8034ed2f9cff7943ad3701e8025328f1e
Kinten Le <kinten108101@protonmail.com>
2026-02-10 21:06:37 +0700

    sdf

--- lua/adwaita-quiet/light.lua
+++ lua/adwaita-quiet/light.lua
@@ -319,6 +319,7 @@ M.set = function()
 		
 		-- fsharp f#
 		hl('@module.fsharp', { fg = colors.orange_5, bold = true })
+		hl('@variable.member.fsharp', { fg = colors.orange_5, bold = true })
 end
 
 return M
