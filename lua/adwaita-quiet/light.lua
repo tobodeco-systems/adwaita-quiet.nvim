9c03098d92202ec8c20d912effd3442fdd1a9128
Kinten Le <kinten108101@protonmail.com>
2026-02-19 05:10:33 +0700

    sdf

--- lua/adwaita-quiet/light.lua
+++ lua/adwaita-quiet/light.lua
@@ -327,11 +327,13 @@ M.set = function()
 		hl('@module.haskell', { fg = colors.orange_5, bold = true })
 		hl('@operator.haskell', { fg = colors.orange_5, bold = true })
 		-- kotlin kt / arrow kotlin arrow-kt
-		hl('@type.kotlin', { fg = colors.teal_5, bold = true })
-		hl('@constructor.kotlin', { fg = colors.teal_5, bold = true })
-    hl('@variable.builtin.kotlin', { fg = colors.orange_4, bold = false })
+		hl('@type.kotlin', { fg = colors.orange_5, bold = true })
+		hl('@constructor.kotlin', { fg = colors.orange_5, bold = true })
+		-- hl('@type.kotlin', { fg = colors.teal_5, bold = true })
+		-- hl('@constructor.kotlin', { fg = colors.teal_5, bold = true })
 		-- hl('@type.kotlin', { fg = colors.purple_3, bold = true })
 		-- hl('@constructor.kotlin', { fg = colors.purple_3, bold = true })
+    hl('@variable.builtin.kotlin', { fg = colors.orange_4, bold = false })
 		hl('@operator.kotlin', { fg = colors.orange_5, bold = true })
 end
 
