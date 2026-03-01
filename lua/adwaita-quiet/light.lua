6cb4bf8aebe01ace42c23410bb1ba32666f97b41
Kinten Le <kinten108101@protonmail.com>
2026-03-02 00:36:28 +0700

    sdf

--- lua/adwaita-quiet/light.lua
+++ lua/adwaita-quiet/light.lua
@@ -330,6 +330,7 @@ M.set = function()
 		hl('@operator.ocaml', { fg = colors.black_3 })
     hl('@constructor.ocaml', { fg = colors.violet_4 })
     hl('@punctuation.property.ocaml', { fg = colors.orange_5 })
+		hl('@function.builtin.ocaml', { link = '@variable.builtin' })
 		-- hl('@variable.parameter.ocaml', { link = 'Identifier' })
 		-- fsharp f#
 		hl('@module.fsharp', { fg = colors.orange_5, bold = true })
